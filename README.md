07f6eed740710819e2cdb81d6bf9cc23b08cb388
BertoLaDK <lucasmulberg@live.dk>
2026-04-17 07:40:46 +0200

    continued work on uart config

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ This repo contains the firmware for the cooling system, along with instructions 
 
 Take a look at the [simple uart config protocol](docs/SIMPLE_UART_CONFIG_PROTOCOL.md) specification to see how to configure the device using UART with a serialport.
 
+For the Ethernet Driver a slightly modified version of the one found in https://github.com/Georges760/STM32_ENC28J60 under [Drivers/Ethernet](https://github.com/Georges760/STM32_ENC28J60/tree/master/Drivers/Ethernet)
--- tasks/communication_task.cpp
+++ tasks/communication_task.cpp
@@ -7,25 +7,8 @@
 
 #include "spi.h"
 #include "etl/string.h"
+#include "etl/vector.h"
 #include "usart.h"
-#include "Ethernet.h"
-
-void ENC28J60_DelayMs(uint32_t delay) {
-    osDelay(delay);
-}
-
-uint32_t ENC28J60_GetMs(void) {
-    return HAL_GetTick();
-}
-
-uint8_t ENC28J60_TransceiveByte(uint8_t data) {
-    uint8_t received;
-    if (HAL_SPI_TransmitReceive(&hspi1, &data, &received, 1, 1000) == HAL_OK) {
-        return received;
-    }
-    return 0;
-}
-
 
 namespace tasks {
     communication_task::communication_task(osMessageQueueId_t ambientTemperatures,
@@ -38,33 +21,6 @@ namespace tasks {
 
     void communication_task::run() {
 
-        static uint8_t mymac[6] = { 0x54, 0x57, 0x51, 0x10, 0x00, 0x25 };
-        //static uint8_t myip[4] = { 192, 168, 0, 15};
-        static uint8_t buf[500];
-        uint16_t plen, dat_p;
-
-        /* For DHCP */
-        static uint8_t myip[4] = { 0, 0, 0, 0 };
-        static uint8_t mynetmask[4] = { 0, 0, 0, 0 };
-
-        // Default gateway. The ip address of your DSL router. It can be set to the same as
-        // websrvip the case where there is no default GW to access the
-        // web server (=web server is on the same lan as this host)
-        static uint8_t gwip[4] = { 192, 168, 0, 1 };
-
-        static uint8_t dnsip[4] = { 0, 0, 0, 0 };
-        static uint8_t dhcpsvrip[4] = { 0, 0, 0, 0 };
-
-        HAL_GPIO_WritePin(ENC_RST_GPIO_Port, ENC_RST_Pin, GPIO_PIN_RESET);
-        osDelay(10);
-        HAL_GPIO_WritePin(ENC_RST_GPIO_Port, ENC_RST_Pin, GPIO_PIN_SET);
-        osDelay(10);
-        Ethernet_Init(mymac);
-
-        if (Ethernet_Revision() <= 0) {
-            // Failed to access ENC28J60
-            while (1); // Just loop here
-        }
         while (true) {
             std::int32_t _last_ambient_temp{};
             std::int32_t _last_internal_temp{};
@@ -93,11 +49,22 @@ namespace tasks {
                     received_str.fill(0);
                     received_str.resize(messageSize);
                     etl::copy_n(uartRxData.data() + 3, messageSize, received_str.begin());
+                    etl::vector<etl::string<20>,10> parameters{};
                     if (received_str.contains(';')) {
-                        //TODO: Make handler for multiple values
+                        auto pos = received_str.find(';');
+                        while (pos != std::string::npos) {
+                            auto parameter = received_str.substr(0, pos);
+                            parameters.push_back(parameter);
+                            received_str.erase(0, pos + 1);
+                            pos = received_str.find(';');
+                        }
+                    } else {
+                        parameters.push_back(received_str);
                     }
-                    auto first_equals = received_str.find('e');
-                    if (first_equals != std::string::npos && received_str.substr(0, first_equals) == "TARGET_TEMP") {
+
+                    auto first_equals = received_str.find('=');
+                    etl::string<64> parameter = received_str.substr(0, first_equals);
+                    if (first_equals != std::string::npos &&  parameter == "TARGET_TEMP") {
                         std::size_t paramEnd = messageSize;
                         if (auto split = received_str.find(";"); split != std::string::npos) {
                             paramEnd = split;
